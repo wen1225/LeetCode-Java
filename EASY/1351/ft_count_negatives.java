5b2654b77e9589e798b63600d3f1b744205277cc
Ken Ho <46903916+wen1225@users.noreply.github.com>
2020-03-24 17:10:18 -0700

    Create ft_count_negatives.java

---
+++ EASY/1351/ft_count_negatives.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        //scan matrix row by row
+        int count = 0;
+        for (int row = 0; row < grid.length; row++){
+            for (int col = 0; col < grid[row].length; col++){
+                if (grid[row][col] < 0)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
