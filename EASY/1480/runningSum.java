c5371fcca61a97654b24d49e52dd1f9f765ab281
Ken Ho <46903916+wen1225@users.noreply.github.com>
2020-07-07 22:16:49 -0700

    Create runningSum.java

---
+++ EASY/1480/runningSum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += sum;
+            sum = nums[i];
+        }
+        return nums;
+    }
+}
